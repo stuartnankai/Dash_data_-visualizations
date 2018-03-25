8f22eae8ae7509cd322d9948f18809ca7d07e552
Jiansun <jians@kth.se>
2018-03-25 19:35:19 +0200

    Add delete function.

--- data_data.py
+++ data_data.py
@@ -11,16 +11,19 @@ import io
 import base64
 import plotly.graph_objs as go
 
-start = time.clock()
+# start = time.clock()
 app = dash.Dash()
 app.scripts.config.serve_locally = True
 app.config.supress_callback_exceptions = True
 temp_df = {}
 
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
 
 def clean_file(df, filename):
-    print("This is filename : ", filename)
-    print("This is temp file name: ", temp_df.keys())
     df = df.iloc[:, :1]
     df.columns = ["tag"]
     df = df[df.tag.isin(["{", "}"]) == False]
@@ -32,14 +35,41 @@ def clean_file(df, filename):
     value_counts = df.tag.value_counts()
 
     df = value_counts.rename_axis('labels').reset_index(name='counts')
-    elapsed = (time.clock() - start)
-    print("Time used:", elapsed)  # It will run three times
-    temp_df[filename] = df
+    # elapsed = (time.clock() - start)
+    temp_df[filename] = df  # save into database
+    return df
+
+
+# Functions
+# file upload function
+def parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+            df = clean_file(df, filename)
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+            df = clean_file(df, filename)
+        else:
+            df = pd.read_fwf(io.StringIO(decoded.decode('iso-8859-1')), sep=" ", header=None)  # For swedish
+            df = clean_file(df, filename)
+    except Exception as e:
+        print(e)
+        return None
+
     return df
 
 
 app.layout = html.Div([
-    html.H2("SIE-file analyser"),
+    html.H2("SIE-file analyser", style={
+        'textAlign': 'center',
+        # 'color': colors['text']
+    }),
     dcc.Upload(
         id='upload-data',
         children=html.Div([
@@ -57,8 +87,9 @@ app.layout = html.Div([
             'margin': '10px'
         },
         multiple=False),
-    html.H5("Updated Table"),
-    html.Div(dt.DataTable(rows=[{}], id='table')),
+    html.H5("Recently Upload Data"),
+    html.Div(dt.DataTable(rows=[{}], id='table', filterable=True, sortable=True)),
+    html.H5("Show Statistics Chart "),
     html.Div(
         [
             dcc.Dropdown(
@@ -67,10 +98,14 @@ app.layout = html.Div([
                     'label': i,
                     # 'value': i
                 } for i in list(temp_df)],
-                value='filename'),
+                # value='filename'
+            ),
         ],
         style={'width': '25%',
                'display': 'inline-block'}),
+    html.Div(id='target'),
+    dcc.Input(id='input', type='text', value='', placeholder='Type the name'),
+    html.Button(id='submit', type='submit', children='Delete'),
     dcc.Graph(id='funnel-graph'),
 ])
 
@@ -95,30 +130,9 @@ def update_graph(filename):
             'layout':
                 go.Layout(
                     title=filename,
-                    barmode='stack')
+                    barmode='stack'
+                )
         }
-
-# Functions
-# file upload function
-def parse_contents(contents, filename):
-    content_type, content_string = contents.split(',')
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-        else:
-            df = pd.read_fwf(io.StringIO(decoded.decode('iso-8859-1')), sep=" ", header=None)  # For swedish
-            df = clean_file(df, filename)
-    except Exception as e:
-        print(e)
-        return None
-
-    return df
 
 
 # callback table creation
@@ -136,13 +150,60 @@ def update_output(contents, filename):
         return [{}]
 
 
-@app.callback(
+@app.callback(  # update the data list
     Output('dropdown', 'options'),
     [Input('upload-data', 'filename')],
-    [State('dropdown', 'options')])
-def update_options(filename, existing_options):
-    existing_options.append({'label': filename, 'value': filename})
+    [State('dropdown', 'options'),
+     State('input', 'value')],
+    [Event('submit', 'click')])
+def update_options(filename, existing_options, state):
+    fileList = [i['label'] for i in existing_options]
+    print("This is file list: ", fileList)
+    print("This is state: ", state)
+    print("This is option: ", existing_options)
+    print("This is file name: ", filename)
+    if state in fileList:
+        del temp_df[state]
+        index_file = fileList.index(state)
+        del existing_options[index_file]
+    else:
+        print("File is not in file list")
+    if filename not in fileList and filename is not None:
+        existing_options.append({'label': filename, 'value': filename})
     return existing_options
+
+
+@app.callback(Output('target', 'children'), [], [State('dropdown', 'options'), State('input', 'value')],
+              events=[Event('submit', 'click')])
+def callback(existing_options, state):
+    fileList = [i['label'] for i in existing_options]
+    if len(state) == 0:
+        if len(fileList) == 0:
+            return "There is no saved database, please upload one"
+        else:
+            return "Please type the name of database."
+    elif state in fileList:
+        return "Database has been deleted: {}".format(state)
+    else:
+        return "Can not find this database: {}".format(state)
+
+
+@app.callback(Output('input', 'value'), [], [State('dropdown', 'options'), State('input', 'value')],
+              events=[Event('submit', 'click')])
+def callback(existing_options, state):
+    fileList = [i['label'] for i in existing_options]
+    if state in fileList:
+        return ""
+    else:
+        return state
+
+
+@app.callback(Output('upload-data', 'filename'), [], [State('dropdown', 'options'), State('input', 'value')],
+              events=[Event('submit', 'click')])
+def callback(existing_options, state):
+    fileList = [i['label'] for i in existing_options]
+    if state in fileList:
+        return ""
 
 
 app.css.append_css({
